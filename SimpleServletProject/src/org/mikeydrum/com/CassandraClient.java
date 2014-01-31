598643cb4345ec2f6421d2096aa7a2e86e11667b
dirtgambit <m.p.hawthorne@dundee.ac.uk>
2014-01-31 20:15:12 +0000

    first build of servlet practice

---
+++ SimpleServletProject/src/org/mikeydrum/com/CassandraClient.java
@@ -0,0 +1,25 @@
+package org.mikeydrum.com;
+//import com.datastax.driver.core.Cluster;
+//import com.datastax.driver.core.Host;
+//import com.datastax.driver.core.Metadata;
+
+public class CassandraClient {
+	
+	//private Cluster cluster;
+	
+	//public void connect(String node) {
+	//      cluster = Cluster.builder()
+	//            .addContactPoint(node).build();
+	 //     Metadata metadata = cluster.getMetadata();
+	 //     System.out.printf("Connected to cluster: %s\n", 
+	 //           metadata.getClusterName());
+	   //   for ( Host host : metadata.getAllHosts() ) {
+	     //    System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
+	       //        host.getDatacenter(), host.getAddress(), host.getRack());
+	   //   }
+	  // }
+	
+	//public CassandraClient() {
+	      
+	  // }
+}
---
+++ SimpleServletProject/src/org/mikeydrum/com/SimpleServlet.java
@@ -0,0 +1,40 @@
+package org.mikeydrum.com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "My First Servlet class hopefully connects to cassandra", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello from GET " + userName);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		String fullName = request.getParameter("fullName");
+		out.println("Your name is " + userName + " " + fullName);
+	}
+
+
+
+
+}
